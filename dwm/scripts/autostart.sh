5457183109cd98da5068e1cd7b93b83bf92a9992
0xnofoot <x0xnofoot@163.com>
2023-05-17 00:54:33 +0800

    update dwm

--- dwm/scripts/autostart.sh
+++ dwm/scripts/autostart.sh
@@ -4,7 +4,7 @@ sh $DWM_HOME/scripts/dualmon.sh
 sh $DWM_HOME/scripts/dwm-status.sh &
 sh $DWM_HOME/scripts/wp-autochange.sh &
 $CLASH_HOME/clash-verge_1.3.1_amd64.AppImage &
-picom --experimental-backends --config $PICOM_HOME/picom.conf &
+picom --experimental-backends --config $DWM_HOME/scripts/picom/picom.conf &
 # picom -b
 xfce4-power-manager &
 xfce4-volumed-pulse &
