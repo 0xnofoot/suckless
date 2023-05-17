62f5eee835e71ec99ed42346f17aa5a63e0d5ff1
0xnofoot <x0xnofoot@163.com>
2023-05-17 13:25:36 +0800

    update dwm

--- dwm/scripts/autostart.sh
+++ dwm/scripts/autostart.sh
@@ -3,7 +3,6 @@
 sh $DWM_HOME/scripts/dualmon.sh
 sh $DWM_HOME/scripts/dwm-status.sh &
 sh $DWM_HOME/scripts/wp-autochange.sh &
-$CLASH_HOME/clash-verge_1.3.1_amd64.AppImage &
 picom --experimental-backends --config $DWM_HOME/scripts/picom/picom.conf &
 # picom -b
 xfce4-power-manager &
--- dwm/scripts/autostart_wait.sh
+++ dwm/scripts/autostart_wait.sh
@@ -5,3 +5,4 @@ sleep 1
 nm-applet &
 libinput-gestures-setup start &
 fcitx5 &
+$CLASH_HOME/clash-verge_1.3.1_amd64.AppImage &
